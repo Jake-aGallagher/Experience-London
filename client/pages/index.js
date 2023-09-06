0dbdc2ac621d6c87c06addf77c651baa2d584553
Jake Gallagher <jake.a.gallagher98@gmail.com>
2023-09-06 20:11:20 +0100

    NextJs client setup

---
+++ client/pages/index.js
@@ -0,0 +1,11 @@
+const Index = () => {
+    return (
+        <div>
+            <div>hello</div>
+            <div>there</div>
+            <div>matey</div>
+        </div>
+    )
+}
+
+export default Index
