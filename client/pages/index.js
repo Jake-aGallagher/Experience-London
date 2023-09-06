072ceef4a279f8c38604e437a32a4b3b6ba435b3
Jake Gallagher <jake.a.gallagher98@gmail.com>
2023-09-06 22:29:20 +0100

    Client side init and authentication

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,18 @@
-const Index = () => {
-    return (
-        <div>
-            <div>hello</div>
-            <div>there</div>
-            <div>matey</div>
-        </div>
-    )
-}
+import buildClient from "../api/build-client";
 
-export default Index
+const Index = ({ currentUser }) => {
+  return currentUser ? (
+    <h1>signed in</h1>
+  ) : (
+    <h1>signed out</h1>
+  );
+};
+
+Index.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
+
+  return data;
+};
+
+export default Index;
